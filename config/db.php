47b3afc10c1025c7518a180d5f40e9d262e10ce6
nikitab <nikitab@ronis.email>
2018-08-16 10:21:27 +0300

    Init commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=mysql-base.db;dbname=dev_nikitab_branch0',
+    'username' => 'dev',
+    'password' => 'devweb',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
